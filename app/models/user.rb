47d5948b53a67e5bc650b456d68fc6e2e2f0bd0f
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-02-09 12:33:30 +0900

    詳細ページルーティング設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }   
+
+  has_many :alcohols
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "toppage#index"
-  resources :alcohols, only: [:index,:new, :create]
+  resources :alcohols, only: [:index,:new, :create, :show]
 end
