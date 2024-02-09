c7d508dea54d7a418d7868ac0b939b8afa47da8c
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-02-09 11:13:56 +0900

    バリデーションの設定

--- app/models/alcohol.rb
+++ app/models/alcohol.rb
@@ -1,4 +1,9 @@
 class Alcohol < ApplicationRecord
   has_one :category
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :alcohol_name, presence: true
+  validates :memo, presence: true
+  validates :category_id, presence: true
 end
