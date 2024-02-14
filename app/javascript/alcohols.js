32756a2f6ec3f8998b94f25e4c5d3253fa6b58a2
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-02-14 12:33:56 +0900

    ポストボタンのJavaScriptの追加

---
+++ app/javascript/alcohols.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const postButton = document.getElementById('postButton');
+
+  postButton.addEventListener('mouseover', function() {
+    postButton.style.backgroundColor = 'lightgray';
+    postButton.style.color = 'maroon'; 
+  });
+
+  postButton.addEventListener('mouseout', function() {
+    postButton.style.backgroundColor = '';
+    postButton.style.color = '';
+  });
+});
