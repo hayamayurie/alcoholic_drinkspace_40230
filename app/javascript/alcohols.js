df514b31c52da63043e8cab94b37527b4ec24912
hayamayurie <154402647+hayamayurie@users.noreply.github.com>
2024-02-14 14:00:09 +0900

    リンクボタンの調整

--- app/javascript/alcohols.js
+++ app/javascript/alcohols.js
@@ -2,12 +2,30 @@ document.addEventListener('turbo:load', function() {
   const postButton = document.getElementById('postButton');
 
   postButton.addEventListener('mouseover', function() {
-    postButton.style.backgroundColor = 'lightgray';
-    postButton.style.color = 'maroon'; 
+    postButton.style.backgroundColor = 'lightyellow';
+    postButton.style.color = 'dimgray'; 
   });
 
   postButton.addEventListener('mouseout', function() {
     postButton.style.backgroundColor = '';
     postButton.style.color = '';
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const categoryButtons = document.querySelectorAll('.wapper');
+
+  categoryButtons.forEach(function(categoryButton) {
+    categoryButton.addEventListener('mouseover', function() {
+      categoryButton.style.backgroundColor = 'lightyellow';
+      categoryButton.style.color = 'dimgray';
+    });
+
+    categoryButton.addEventListener('mouseout', function() {
+      categoryButton.style.backgroundColor = '';
+      categoryButton.style.color = '';
+    });
   });
 });
